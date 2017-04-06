780dc2f00f2f72dc0fb1d9876340ddcd66b544b0
Headbangeerr <1078692034@qq.com>
2017-04-06 21:42:19 +0800

    更新

--- src/com/bbsforum/dao/BoardDAO.java
+++ src/com/bbsforum/dao/BoardDAO.java
@@ -3,10 +3,15 @@ package com.bbsforum.dao;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * 
+ * @author 颜磊
+ *@date 2017年4月6日
+ */
 public interface BoardDao {
 
 	//用于页面读取时获取板块列表
-	public List getBoardList();
+	public List getBoardList(  );
 	//获取子版块列表，需要传入父版块Id
 	public Set getChildBoardListByParentBoardId(int ParentBoardId);
 	//删除指定id的子版块
